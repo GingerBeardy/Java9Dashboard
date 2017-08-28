1f075f11f0826a42e7248e38c1eebdce66e5d5b7
Eric Driessen <eric_driessen@live.nl>
2017-08-28 20:56:28 +0200

    Line graph seems to work...

---
+++ nl.group9.java9.dashboard/src/nl/group9/java9/dashboard/revenuegraph/RevenueChartController.java
@@ -0,0 +1,40 @@
+package nl.group9.java9.dashboard.revenuegraph;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.util.StringConverter;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import static java.lang.Integer.valueOf;
+import static java.lang.String.format;
+
+public class RevenueChartController implements Initializable {
+
+    @FXML
+    private NumberAxis yAxis;
+    @FXML
+    private LineChart<String, Double> revenueOverTime;
+
+    public void setData(RevenueChartData data) {
+        revenueOverTime.getData().add(data.getDataSeries());
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        yAxis.setTickLabelFormatter(new StringConverter<Number>() {
+            @Override
+            public String toString(Number object) {
+                return format("%.0f", object);
+            }
+
+            @Override
+            public Number fromString(String string) {
+                return valueOf(string);
+            }
+        });
+    }
+}
---
+++ nl.group9.java9.dashboard/src/nl/group9/java9/dashboard/revenuegraph/RevenueChartData.java
@@ -0,0 +1,57 @@
+package nl.group9.java9.dashboard.revenuegraph;
+
+import javafx.scene.chart.XYChart;
+import javafx.scene.chart.XYChart.Data;
+import nl.group9.java9.api.domain.Sale;
+import nl.group9.java9.dashboard.observable.SalesObserver;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+import static java.time.LocalTime.now;
+
+public class RevenueChartData implements SalesObserver {
+    private final XYChart.Series<String, Double> dataSeries = new XYChart.Series<>();
+    private final Map<Integer, Integer> minuteToDataPosition = new HashMap<>();
+
+    public RevenueChartData() {
+        // get minute value for right now
+        int nowMinute = LocalDateTime.now().getMinute();
+
+        // create an empty bar for every minute for the next ten minutes
+        IntStream.range(nowMinute, nowMinute + 10)
+                .forEach(this::initialiseBarToZero);
+    }
+
+    @Override
+    public void onSalesUpdate(List<Sale> sales) {
+        int x = now().getMinute();
+        Integer dataIndex = minuteToDataPosition.get(x);
+
+        Map<Integer, Integer> aggregatedSalesPerMinute = new HashMap<>();
+        for (Sale s : sales) {
+            int minute = s.getTimeOfSale().getMinute();
+            if (!aggregatedSalesPerMinute.containsKey(minute)) {
+                aggregatedSalesPerMinute.put(minute, 0);
+            }
+            double sum = aggregatedSalesPerMinute.get(minute) + s.getRevenue();
+            aggregatedSalesPerMinute.put(minute, Math.toIntExact(Math.round(sum)));
+        }
+
+        Data<String, Double> barForNow = dataSeries.getData().get(dataIndex);
+        barForNow.setYValue(Double.valueOf(aggregatedSalesPerMinute.get(x)));
+    }
+
+    public XYChart.Series<String, Double> getDataSeries() {
+        return dataSeries;
+    }
+
+    private void initialiseBarToZero(int minute) {
+        dataSeries.getData().add(new Data<>(String.valueOf(minute), 0.0));
+        minuteToDataPosition.put(minute, dataSeries.getData().size() - 1);
+    }
+}
+
